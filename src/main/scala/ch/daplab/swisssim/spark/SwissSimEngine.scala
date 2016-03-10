08da1041314275e7c1330227c3f519e96e71b47b
Perroud, Benoit <benoit@noisette.ch>
2016-03-10 21:47:58 +0100

    Oups, this count is just a killer (it is the first action on the RDD, which triggers the load from Cassandra in the driver!!)

--- src/main/scala/ch/daplab/swisssim/spark/SwissSimEngine.scala
+++ src/main/scala/ch/daplab/swisssim/spark/SwissSimEngine.scala
@@ -269,7 +269,7 @@ object SwissSimEngine extends App {
       .filter( t => t._1 >= DEFAULT_SIMILARITY_THRESHOLD)
 
     println(tanimotorrd.toDebugString)
-    println(tanimotorrd.count)
+    //println(tanimotorrd.count)
 
     val topN = tanimotorrd.top(DEFAULT_NUMBER_TO_RETURN)(MoleculeSimilarityOrdering)
 
